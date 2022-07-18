13def4334cc1e0c0a426f24e97be9d61b55f0435
zaenury <adhiimzaenury@gmail.com>
2022-07-18 14:37:43 +0800

    Initial Commit

---
+++ myswift/myswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  myswiftApp.swift
+//  myswift
+//
+//  Created by Oddbit MBP on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct myswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
